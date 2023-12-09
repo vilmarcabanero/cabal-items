1e9dedb12d5fe9ca892fed75db4b945dc54221d7
Vil Depanes <vilmarcabanero@gmail.com>
2023-12-09 20:29:38 +0800

    Update price colors

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -10,16 +10,22 @@ const Price: React.FC<PriceProps> = ({ amount }) => {
     // Determine the color based on the value range
     if (amount >= 1e10) {
       // Tens of billions
-      return "#00FFFF"; // Cyan color
+      return "#74cc93";
     } else if (amount >= 1e9) {
       // Billions
-      return "#000000"; // Black color
+      return "#5a6497"; // Black color
     } else if (amount >= 1e8) {
       // Hundreds of millions
       return "#b3653e"; // Brown color
     } else if (amount >= 1e7) {
       // Tens of millions
       return "#0B6623"; // Forest green color
+    } else if (amount >= 1e6) {
+      // millions
+      return "#47e0ee";
+    } else if (amount >= 1e5) {
+      // hundred thousands
+      return "#d0be70";
     }
     // Default color if none of the conditions are met
     return "inherit";
